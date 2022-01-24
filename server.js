36790cd7eeddfb52708d5298100292f1bfa11d63
Parth Shah <parth.shah2118@gmail.com>
2022-01-24 17:57:39 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');// load the express library
+const app = express();// create an instance of the library
+
+app.get('/hello', (req, res) =>// use express library to listen for URL pattern "/hello"
+  res.send('Hello World!'));// respond with string "Hello World!"
+
+const PORT = 4000;
+app.listen(PORT);// listen to port 4000
